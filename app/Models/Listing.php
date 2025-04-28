ef5f6731880faab9d9e0fe84c683161cc618740d
nasiralibaloch <nasiralibaloch110@gmail.com>
2025-04-28 16:05:32 +0500

    Laravel Project

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -16,18 +16,12 @@ class Listing extends Model
         if($filters['tag'] ?? false){
             $query->where('tags','like','%' .request('tag') . '% ');
         }
-        if($filters['search'] ?? false){
-            
+        if($filters['search'] ?? false){    
             $query->where('title','like', '%' .request('search') . '%' )
             ->orWhere('description','like','%' .request('search') . '%')
-            ->orWhere('tags','like','%' .request('search') . '%');
-            
+            ->orWhere('tags','like','%' .request('search') . '%'); 
         }
-       
-
     } 
-
-
     protected $fillable = ['title', 'description', 'user_id'];
 
 
